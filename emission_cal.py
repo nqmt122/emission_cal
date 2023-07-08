ca3331aa75437f4c52945797d1905f487507d494
Nguyen Quang Minh Tri <79180376+nqmt122@users.noreply.github.com>
2023-07-08 10:41:12 +0700

    Update emission_cal.py

--- emission_cal.py
+++ emission_cal.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import pandas as pd
+import requests
+
+# Create an empty DataFrame to store the records
+record_df = pd.DataFrame(columns=["Start", "Destination", "Cargo Weight (kg)", "Truck Weight (kg)", "CO2 Emissions"])
 
 # Data
 fuel_lookup = {
@@ -11,38 +15,69 @@ fuel_lookup = {
 df = pd.DataFrame(fuel_lookup)
 
 # Calculation
-
-def co2_cal(distance, cargo_weight,truck_weight):
-    #write code to filter truck_type_do_rate by truck_weight
+def co2_cal(distance, cargo_weight, truck_weight):
     def get_fuel(truck):
         mask = (truck >= df['min_ton']) & (truck <= df['max_ton'])
         if mask.any():
             return df.loc[mask, 'avg_fuel'].iloc[0]
         return None
+
     truck_type_do_rate = get_fuel(truck_weight)
-    #cargo_do_rate
     cargo_do_rate = cargo_weight * 1.3
-    #total_fuel
-    total_fuel = (truck_type_do_rate + cargo_do_rate) * distance/100
-    co2_emissions = total_fuel * 2.68   
+    total_fuel = (truck_type_do_rate + cargo_do_rate) * distance / 100
+    co2_emissions = total_fuel * 2.68
+
     return co2_emissions
 
-# Call
+def get_distance(start, destination):
+    url = "https://maps.googleapis.com/maps/api/distancematrix/json"
 
+    params = {
+        "origins": start,
+        "destinations": destination,
+        "key": "YOUR_GOOGLE_MAPS_API_KEY"
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    if "rows" in data and data["rows"][0]["elements"][0]["status"] == "OK":
+        distance_text = data["rows"][0]["elements"][0]["distance"]["text"]
+        distance_value = data["rows"][0]["elements"][0]["distance"]["value"] / 1000  # Convert to kilometers
+        return distance_value
+    else:
+        return None
+
+# Call
 def main():
     st.title("CO2 Emissions Calculator")
-    
+
     # Input variables
-    distance = st.number_input("Distance (in km)", min_value=0.0, step=0.1)
+    start = st.text_input("Start Location")
+    destination = st.text_input("Destination Location")
     cargo_weight = st.number_input("Cargo Weight (in kg)", min_value=0.0, step=1.0)
     truck_weight = st.number_input("Truck Weight (in kg)", min_value=0.0, step=1.0)
 
-    # Calculate CO2 emissions
-    co2_emissions = co2_cal(distance, cargo_weight, truck_weight)
+    # Calculate distance using Google Maps API
+    distance = get_distance(start, destination)
 
-    # Display the result
-    st.subheader("Result")
-    st.write("CO2 Emissions:", co2_emissions)
+    if distance is not None:
+        # Calculate CO2 emissions
+        co2_emissions = co2_cal(distance, cargo_weight, truck_weight)
+
+        # Display the result
+        st.subheader("Result")
+        st.write("Distance:", distance, "km")
+        st.write("CO2 Emissions:", co2_emissions)
+
+        # Record the result in the DataFrame
+        record_df.loc[len(record_df)] = [start, destination, cargo_weight, truck_weight, co2_emissions]
+
+        # Display the record table
+        st.subheader("Record")
+        st.table(record_df)
+    else:
+        st.error("Failed to retrieve distance from the Google Maps API.")
 
 if __name__ == "__main__":
     main()
